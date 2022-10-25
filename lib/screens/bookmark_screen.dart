4d87dde7e62e77978c88a581e41ebe32892d8937
akshay@96804 <19uec032@lnmiit.ac.in>
2022-10-25 14:16:16 +0530

    minor changes

--- lib/screens/bookmark_screen.dart
+++ lib/screens/bookmark_screen.dart
@@ -1,6 +1,8 @@
+import 'package:book_rent_app/screens/bookDetailsScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class BookMarkScreen extends StatelessWidget {
   // const BookMarkScreen({ Key? key }) : super(key: key);
@@ -11,7 +13,9 @@ class BookMarkScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("My BookMarks",style: TextStyle(color: Colors.black,fontSize: 18.0))
+      ),
       body: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
         builder: (context, snapshot) {
           if(snapshot.connectionState == ConnectionState.waiting){
@@ -31,19 +35,28 @@ class BookMarkScreen extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          CircleAvatar(),
+                          CircleAvatar(
+                                                                       child: Icon(Icons.person),
+ 
+                          ),
                           Text(snapshot.data.docs[index].data()['owner_name'])
                         ],
                       ),
                       GestureDetector(
                         onTap: () {
-                          // Navigator.push(
-                          //     context,
-                          //     PageTransition(
-                          //         child: DemoBookDetail(
-                          //             snapshot.data.docs[index].id),
-                          //         type: PageTransitionType
-                          //             .rightToLeft));
+                          Navigator.push(context, PageTransition(child: BooksDetailsScreen(
+                            authorName: snapshot.data.docs[index].data()['author_name'],
+                            bookImg: snapshot.data.docs[index].data()['bookImg'],
+                            bookName: snapshot.data.docs[index].data()['book_name'],
+                            branch: snapshot.data.docs[index].data()['branch'],
+                            courseName: snapshot.data.docs[index].data()['course_name'],
+                            description: snapshot.data.docs[index].data()['description'],
+                            docId: snapshot.data.docs[index].data()['docId'],
+                            owner: snapshot.data.docs[index].data()['owner'],
+                            ownerUid: snapshot.data.docs[index].data()['owner_uid'],
+                            price: snapshot.data.docs[index].data()['price'],
+                            year: snapshot.data.docs[index].data()['year'],
+                          ), type: PageTransitionType.rightToLeft));
                         },
                         child: Container(
                           height: MediaQuery.of(context).size.height * 0.4,
